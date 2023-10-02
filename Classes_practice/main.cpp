79f946e8f325760c0229576278b284f14551fcbb
Leonid <rastrast207@gmail.com>
2023-10-02 21:49:14 +0300

    Play with inheritance a little bit

--- Classes_practice/main.cpp
+++ Classes_practice/main.cpp
@@ -2,8 +2,12 @@
 #include <conio.h>
 #include <iostream>
 #include <map>
+#include <iomanip>
+#include <string>
+#include <string.h>
 
 using namespace std;
+const int LEN = 80;
 void one_line();
 void two_lines();
 int count_of_employees = 0;
@@ -38,33 +42,84 @@ public:
 class employee {
 private:
 	int number;
+	char name[LEN];
 	float salary;
 	date date_start;
 	etype post;
 public:
 	employee(): number(0), salary(0) {}
 	employee(int n, float s): number(n), salary(s) {}
-	void add_emp();
-	void show_emp()const; 
+	void get_data();
+	void put_data()const; 
+};
+
+class _manager : public employee {
+private:
+	char title[LEN];
+	double dues;
+public:
+	void get_data() {
+		employee::get_data();
+		cout << "Enter the manager's title: "; cin.ignore(); cin >> title;
+		cout << "Enter the sum of dues to the golf club: "; cin >> dues;
+	}
+	void put_data() {
+		employee::put_data();
+		cout << "Title: " << title << endl;
+		cout << "Sum of dues: " << dues << endl;
+	}
+};
+
+class scientist : public employee {
+private:
+	int pubs;
+public:
+	void get_data() {
+		employee::get_data();
+		cout << "Enter a count of publications: "; cin >> pubs;
+	}
+	void put_data() {
+		employee::put_data();
+		cout << "Count of publications: " << pubs << endl;
+	}
+};
+
+class _laborer : public employee {
+	//		========D
+	//	-(.)(.)-             it's a gun u know
 };
 
 /////////////////////////////////////////////////////////////////////////////////////////
+
 int main() {
-	employee e1, e2, e3;
-	e1.add_emp();
-	e2.add_emp();
-	e3.add_emp();
-	e1.show_emp();
-	e2.show_emp();
-	e3.show_emp();
+	_manager m1;
+	scientist s1;
+	_laborer l1;
+	m1.get_data();
+	s1.get_data();
+	l1.get_data();
+
+	m1.put_data();
+	s1.put_data();
+	l1.put_data();
+
+	//employee e1, e2, e3;
+	//e1.get_data();
+	//e2.add_emp();
+	//e3.add_emp();
+	//e1.put_data();
+	//e2.show_emp();
+	//e3.show_emp();
 	return 0;
 }
 /////////////////////////////////////////////////////////////////////////////////////////
 
-void employee::add_emp() {
+void employee::get_data() {
 	count_of_employees++;
 	cout << "Enter the employee " << count_of_employees << " number: ";
 	cin >> number;
+	cout << "Enter the employee " << count_of_employees << " name: ";
+	cin.ignore(); cin.get(name, LEN);
 	cout << "Enter the employee " << count_of_employees << " salary: ";
 	cin >> salary;
 	cout << "Enter the employee " << count_of_employees << " start day ";
@@ -87,9 +142,10 @@ void employee::add_emp() {
 	one_line();
 }
 
-void employee::show_emp()const {
+void employee::put_data()const {
 	two_lines();
 	cout << "Number: " << number << endl;
+	cout << "Name: " << name << endl;
 	cout << "Salary: " << salary << " $" << endl;
 	cout << "Start date: "; date_start.show_date(); cout << endl;
 	cout << "Post: " << post << endl;
